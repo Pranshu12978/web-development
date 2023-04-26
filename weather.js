7ffd63ceb217c62ce14d59d2f4b7c57c81dfd92c
Pranshu12978 <121747639+Pranshu12978@users.noreply.github.com>
2023-04-26 13:12:19 +0530

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('form').submit(function(event) {
+		event.preventDefault();
+		var location = $('#location').val();
+		var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=API_KEY&units=metric';
+		$.getJSON(url, function(data) {
+			var temp = data.main.temp;
+			var desc = data.weather[0].description;
+			var icon = data.weather[0].icon;
+			var iconUrl = 'http://openweathermap.org/img/w/' + icon + '.png';
+			$('#weather').html('<p>Temperature: ' + temp + ' &deg;C</p><p>Description: ' + desc + '</p><img src="' + iconUrl + '">');
+		});
+	});
+});
