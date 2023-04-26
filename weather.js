e0621abbca16bc642b747bf810c9c90dad91ee04
Pranshu12978 <121747639+Pranshu12978@users.noreply.github.com>
2023-04-26 21:32:54 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('form').submit(function(event) {
 		event.preventDefault();
 		var location = $('#location').val();
-		var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=202dd79001cf76ca1878bed8b00ea2fe=metric';
+		var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=202dd79001cf76ca1878bed8b00ea2fe&units=metric';
 		$.getJSON(url, function(data) {
 			var temp = data.main.temp;
 			var desc = data.weather[0].description;
